9c9cd476ee45677ee14dfccbcd762de8a803ade5
Ivan Golubić <94765726+Igol4@users.noreply.github.com>
2022-10-29 16:40:38 +0200

    Update igraj.js

--- slash/igraj.js
+++ slash/igraj.js
@@ -18,63 +18,17 @@ module.exports = {
 
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
-                searchEmfine: QueryType.AUTO //YOUTUBE_VIDEO
+                searchEmfine: QueryType.AUTO
             })
             if (result.tracks.length === 0)
                 return interaction.editReply("Niš od tvoje pjesme, nema je.")
             const pjesma = result.tracks[0]
             await queue.addTrack(pjesma)
-            //console.log(queue)
-            //console.log(`Pjesma broj 1 je ${queue.tracks[0]}`)
             embed
                 .setDescription(`**[${pjesma.title}](${pjesma.url})** je dodana u red`)
                 .setThumbnail(pjesma.thumbnail)
                 .setFooter({ text: `Trajanje: ${pjesma.duration}`})
-            /*if (interaction.options.getSubcommand() === "pjesma"){
-                let url = interaction.options.getString("url")
-                const result = await client.player.search(url, {
-                    requestedBy: interaction.user,
-                    searchEmfine: QueryType.AUTO //YOUTUBE_VIDEO
-                })
-                if (result.tracks.length === 0)
-                    return interaction.editReply("Ništa od pjesme")
-                const pjesma = result.tracks[0]
-                await queue.addTrack(pjesma)
-                embed
-                    .setDescription(`**[${pjesma.title}](${pjesma.url})** je dodana u red`)
-                    .setThumbnail(pjesma.thumbnail)
-                    .setFooter({ text: `Trajanje: ${pjesma.duration}`})
-            } 
-            else if (interaction.options.getSubcommand() === "playlista"){
-                let url = interaction.options.getString("url")
-                const result = await client.player.search(url, {
-                    requestedBy: interaction.user,
-                    searchEmfine: QueryType.AUTO //YOUTUBE_PLAYLIST
-                })
-                if (result.tracks.length === 0)
-                    return interaction.editReply("Ništa od playliste")
-                const playlista = result.playlista
-                await queue.addTracks(result.tracks)
-                embed
-                    .setDescription(`**${result.tracks.length} pjesama s playliste [${playlista.title}](${playlista.url})** je dodano u red`)
-                    .setThumbnail(playlista.thumbnail)
-            }
-            else if (interaction.options.getSubcommand() === "pretraživanje"){
-                let url = interaction.options.getString("pretraživanje")
-                const result = await client.player.search(url, {
-                    requestedBy: interaction.user,
-                    searchEmfine: QueryType.AUTO
-                })
-                if (result.tracks.length === 0)
-                    return interaction.editReply("Ništa od playliste")
-                const pjesma = result.tracks[0]
-                await queue.addTrack(pjesma)
-                embed
-                    .setDescription(`**[${pjesma.title}](${pjesma.url})** je dodana u red`)
-                    .setThumbnail(pjesma.thumbnail)
-                    .setFooter({ text: `Trajanje: ${pjesma.duration}`})
-            }*/
-            if (!queue.playing) await queue.play() //mozda nije unutar prave zagrade
+            if (!queue.playing) await queue.play()
             await interaction.editReply({
                 embeds: [embed]
             })
